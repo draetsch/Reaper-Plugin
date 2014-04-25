4932ea1f6a83d334729451d1ea6114aaeb4bd2e7
Malte Dreschert <malte.dreschert@gmail.com>
2014-04-25 18:39:43 +0200

    beginnings of chapter export

---
+++ ultraschall/reaper_ninjamloop/ExportChapters.cpp
@@ -0,0 +1,9 @@
+//
+//  ExportChapters.cpp
+//  reaper_Ultraschall
+//
+//  Created by Malte Dreschert on 25.04.14.
+//
+//
+
+#include "ExportChapters.h"
---
+++ ultraschall/reaper_ninjamloop/ExportChapters.h
@@ -0,0 +1,14 @@
+//
+//  ExportChapters.h
+//  reaper_Ultraschall
+//
+//  Created by Malte Dreschert on 25.04.14.
+//
+//
+
+#ifndef __reaper_Ultraschall__ExportChapters__
+#define __reaper_Ultraschall__ExportChapters__
+
+#include <iostream>
+
+#endif /* defined(__reaper_Ultraschall__ExportChapters__) */
