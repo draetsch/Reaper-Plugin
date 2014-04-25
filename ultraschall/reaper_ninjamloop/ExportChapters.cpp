663fd79a79b288cc42479d8c678e4a2a50b33645
Malte Dreschert <malte.dreschert@gmail.com>
2014-04-25 18:40:00 +0200

    beginnings of chapter export 2

--- ultraschall/reaper_ninjamloop/ExportChapters.cpp
+++ ultraschall/reaper_ninjamloop/ExportChapters.cpp
@@ -7,3 +7,24 @@
 //
 
 #include "ExportChapters.h"
+//#include "../reaper_plugin.h"
+//#include "functions.h"
+
+using namespace tinyxml2;
+
+void exportChapters()
+{
+    
+    //auto jj = CountProjectMarkers(0, NULL, NULL);
+    
+    
+    XMLDocument * doc = new XMLDocument();
+    XMLElement * root = doc->NewElement("Chapters");
+    
+    doc->InsertEndChild(root);
+    
+    //double time = parse_timestr("sdd");
+    
+    doc->SaveFile("/Users/Malte/___hhh.xml");
+    auto h = 0;
+}
--- ultraschall/reaper_ninjamloop/ExportChapters.h
+++ ultraschall/reaper_ninjamloop/ExportChapters.h
@@ -10,5 +10,10 @@
 #define __reaper_Ultraschall__ExportChapters__
 
 #include <iostream>
+#include "tinyxml2.h"
+//#include "reaper_plugin_functions.h"
+
+
+void exportChapters();
 
 #endif /* defined(__reaper_Ultraschall__ExportChapters__) */
