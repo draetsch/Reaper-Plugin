36f4f2862a5f37547041d5f9785026bd22192b93
Daniel Lindenfelser <daniel@lindenfelser.de>
2014-06-21 20:11:21 +0200

    add new plugin project

---
+++ Reaper Plugin/Reaper PluginTests/Reaper_PluginTests.m
@@ -0,0 +1,34 @@
+//
+//  Reaper_PluginTests.m
+//  Reaper PluginTests
+//
+//  Created by Daniel Lindenfelser on 21.06.14.
+//  Copyright (c) 2014 Ultraschall. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Reaper_PluginTests : XCTestCase
+
+@end
+
+@implementation Reaper_PluginTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
